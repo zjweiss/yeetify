be4957e2011ee7af9a02f5076acb5f6cbdd8f36c
muralim <y2kmurali@gmail.com>
2019-05-02 22:17:49 -0400

    Add error checking in commandline inputs
    Check for invalid commandline args and invalid filenames

--- yeetify.cpp
+++ yeetify.cpp
@@ -8,6 +8,9 @@ using namespace std;
 //Iterates through source code and finds each unique string in file, adding it to the map.
 void tokenize_file(const string &source, vector<string> &tokens){
     ifstream fin(source+".cpp");
+    if(!fin.good()){
+        throw source;
+    }
     string token;
 
     //iterate until #includes or using directives are passed, since >> operator skips whitespace
@@ -25,14 +28,22 @@ int main(int argc, char* argv[]){
     //checks inputs and gathers user intent
     if(argc != 2){
         cout << "Usage: ./yeetify.exe FILENAME_NO_EXTENSION" << endl;
+        return 1;
     }
+
+
     string filename = string(argv[1]);
 
     vector<string> tokens;
     map<string, string> yeet_map;
     //tokenize the file
-    tokenize_file(filename, tokens);
-
+    try{
+        tokenize_file(filename, tokens);
+    }
+    catch(string s){
+        cout << "Error reading file: " << s << ".cpp" << endl;
+        return 1;
+    }
     //determines how many unique permutations of word will be needed
     int num_yeets = tokens.size();
     cout << num_yeets << endl;
